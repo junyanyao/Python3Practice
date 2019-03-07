4bab8e3bd12ba00d34bf08f570acbbdae694c72f
junyanyao <yaojunyan1202@gmail.com>
2019-03-06 21:35:05 -0500

    add challenge problem

--- level2_3.5.py
+++ level2_3.5.py
@@ -78,3 +78,9 @@ SPY GAME: Write a function that takes in a list of integers and returns True if 
 '''
 
 
+def spy_game(nums):
+    code =[0,0,7,'x']
+    for num in nums:
+        if num == code[0]:
+            code.pop(0)
+    return len(code)==1
